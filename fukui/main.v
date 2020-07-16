38bd2fe057109d072cacb35d398dccd3c3b5190f
coco <mrkokipro@icloud.com>
2020-07-16 22:17:39 +0900

    runner分け

--- fukui/main.v
+++ fukui/main.v
@@ -1,7 +1,28 @@
-module GenComp4D();
+module GenComp4D(
+	input [3:0] Q1000,
+	input [3:0] Q0100,
+	input [3:0] Q0010,
+	input [3:0] Q0001,
+	input [3:0] V1000,
+	input [3:0] V0100,
+	input [3:0] V0010,
+	input [3:0] V0001,
+	output EQU
+);
+	wire [13:0] HEXQ;
+	wire [13:0] HEXV;
 
+  assign HEXQ = 14'd1000 * Q1000 + 14'd0100 * Q0100 + 14'd0010 * Q0010 + Q0001;
+  assign HEXV = 14'd1000 * V1000 + 14'd0100 * V0100 + 14'd0010 * V0010 + V0001;
+  assign EQU = (HEXV <= HEXQ);
+
+endmodule
+
+
+module Runner();
+	integer i;
   reg CLK;
-	//inputs
+
 	reg [3:0] Q1000;
 	reg [3:0] Q0100;
 	reg [3:0] Q0010;
@@ -10,27 +31,13 @@ module GenComp4D();
 	reg [3:0] V0100;
 	reg [3:0] V0010;
 	reg [3:0] V0001;
-
-	//outputs
-	wire EQU;
-
-	//wire
-	wire [13:0] HEXQ;
-	wire [13:0] HEXV;
-
-	//i
-	integer i1000;
-	integer i0100;
-	integer i0010;
-	integer i0001;
-
-  assign HEXQ = 14'd1000 * Q1000 + 14'd0100 * Q0100 + 14'd0010 * Q0010 + Q0001;
-  assign HEXV = 14'd1000 * V1000 + 14'd0100 * V0100 + 14'd0010 * V0010 + V0001;
-  assign EQU = (HEXV <= HEXQ);
+  wire EQU;
+  
+  GenComp4D genComp4D(.Q1000(Q1000), .Q0100(Q0100), .Q0010(Q0010), .Q0001(Q0001),
+                      .V1000(V1000), .V0100(V0100), .V0010(V0010), .V0001(V0001), .EQU(EQU));
 
   initial begin
     CLK = 0;
-
     V1000 = 1;
     V0100 = 2;
     V0010 = 3;
@@ -39,52 +46,44 @@ module GenComp4D();
     Q0100 = 0;
     Q0010 = 0;
     Q0001 = 0;
-    forever #1000000 begin
+    forever #1 begin
       CLK = ~CLK;
     end
   end
 
-  always begin: exit
-    if (EQU == 1) begin
-      Q1000 = 4'd0000;
-      Q0100 = 4'd0000;
-      Q0010 = 4'd0000;
-      Q0001 = 4'd0000;
-    end else begin
-      for (i1000=0 ; i1000<2 ; i1000=i1000+1) begin
-        for (i0100=0 ; i0100<=9 ; i0100=i0100+1) begin
-          for( i0010=0 ; i0010<=9 ; i0010=i0010+1) begin
-            for (i0001=0 ; i0001<=9 ; i0001=i0001+1) begin
-              Q0001 = Q0001 + 4'd0001;
-              #1000000;
-              if (EQU == 1) disable exit;
-            end
-            if (EQU == 1) disable exit;
-            Q0001 = 0;
-            Q0010 = Q0010 + 4'd0010;
-            #1000000;
-            if (EQU == 1) disable exit;
-          end
-          if (EQU == 1) disable exit;
-          Q0010 = 0;
-          Q0100 = Q0100 + 4'd0100;
-          #1000000;
-          if (EQU == 1) disable exit;
-        end
-        if (EQU == 1) disable exit;
-        Q0100 = 0;
-        Q1000 = Q1000 + 4'd1000;
-        #1000000;
-        if (EQU == 1) disable exit;
-      end
-    end
+  initial begin
+    #2000 $finish();
   end
 
   initial begin
-      $dumpfile("Main.vcd");
-      $dumpvars(0, CLK);
-      $dumpvars(0, EQU);
+    $dumpfile("Main.vcd");
+    $dumpvars(0, CLK);
+    $dumpvars(0, Q1000);
+    $dumpvars(0, Q0100);
+    $dumpvars(0, Q0010);
+    $dumpvars(0, Q0001);
+    $dumpvars(0, V1000);
+    $dumpvars(0, V0100);
+    $dumpvars(0, V0010);
+    $dumpvars(0, V0001);
+    $dumpvars(0, EQU);
   end
 
+  always begin
+    i = 0;
+    Q1000 = 4'd0;
+    Q0100 = 4'd0;
+    Q0010 = 4'd0;
+    Q0001 = 4'd0;
+    while (~EQU) begin
+      Q0001 = i%10;
+      Q0010 = (i%100-Q0001)/10;
+      Q0100 = (i%1000-Q0010-Q0001)/100;
+      Q1000 = (i-Q0100-Q0010-Q0001)/1000;
+      $write("[%t] Q: %d%d%d%d\n", $time, Q1000, Q0100, Q0010, Q0001);
+      // $write("[%t] EQU: %b\n", $time, EQU);
+      i += 1;
+      #1;
+    end
+  end
 endmodule
-
